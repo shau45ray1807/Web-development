ef0ada955cb05d5411355bc5e196646af3ccb081
shau45ray1807 <shau45ray1807@gmail.com>
2026-06-01 15:20:19 +0530

    Add files via upload

---
+++ video  54/script.js
@@ -0,0 +1,22 @@
+alert("Hello World")
+
+console.log("Code is running......")
+console.log("Code is also running......")
+console.log("Code is like a wow running......")
+
+var a = prompt("Enter your number")
+var istrue = confirm("Are you sure you want to leave this page and blast your computer")
+
+if (istrue) {
+    console.log("Computer is blasting")
+}
+
+else {
+    console.log("Computer is not blasting")
+}
+
+console.log("The number you entered is " + a)
+
+document.title = "Yeh, I'm also good."
+
+document.body.style.backgroundColor = "red"
